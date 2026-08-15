057b314b68148ec5ed507eb3fdc515cc0aab062c
Onion-Lin <3241633984@qq.com>
2026-08-15 10:25:15 +0800

    完成第一周OJ平台开发

---
+++ problems/p04_cmp_eq4/cmp_eq4.v
@@ -0,0 +1,15 @@
+// p04_cmp_eq4 四位相等比较器（数电·组合逻辑（一）比较器(0)）
+// 在 Logisim 中你根据给定结构图，用异或门 + 或非搭出了四位相等比较器（判断相等）。
+// 本题接口固定：4 位 a、b，输出 eq。
+module cmp_eq4(
+    input  logic [3:0] a,
+    input  logic [3:0] b,
+    output logic       eq
+);
+// ===== TODO: 在此完成你的设计 =====
+
+
+
+
+// ===== END =====
+endmodule
